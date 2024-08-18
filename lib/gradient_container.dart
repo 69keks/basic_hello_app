c906d8ae0bd6caf549b96c69caf01456089a9fc2
Leviathan melvilli <mehere.youthere@pm.me>
2024-08-18 22:17:26 +0530

    It's over.. Rushed in a hurry so forgot to individually stage each change...

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/styled_text.dart';
+import 'package:my_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.deepPurple, Colors.purple],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyledText("Hello, World!"),
+        child: DiceRoller(),
       ),
     );
   }
